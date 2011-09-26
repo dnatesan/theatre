a6f551f21b4c4e7027348093f33b89aaeedf9273
Divya Natesan <dnatesan@andrew.cmu.edu>
2011-09-26 03:22:55 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Theatre::Application.config.secret_token = 'df34450e723b572848d2506392e1b259ac59e5768b6dbdc2d74928433f1d08d30b0e3f48ad9efda81868e5a4d33204ec11f17ebb1d10b56cee8d41ad4355222e'
